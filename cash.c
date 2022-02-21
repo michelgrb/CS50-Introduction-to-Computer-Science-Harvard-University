9ade5b053b4debd39de126768a70a9fc54cba15b
Michel <93449395+mlgcode@users.noreply.github.com>
2022-02-21 19:37:52 +0100

    Update cash.c

--- cash.c
+++ cash.c
@@ -1,3 +1,10 @@
+// helps sellers make a change for buyer’s easily, with as few coins as possible
+// Prompt user for an amount of Change
+// if the change is less than 0, reprompt the user until cooperate.
+// Due to floating impression, round the cent to the nearest penny
+// Then use the largest coins possible, keeping track of coins used.
+// Print the number of coins.
+
 #include <stdio.h>
 #include <cs50.h>
 #include <math.h>
